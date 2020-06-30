5722ab4b2652ac2779cd8aff5ce8ebdd4af3d6d0
Philipp <55308835+Filjo0@users.noreply.github.com>
2020-06-30 11:24:49 +0800

    Add files via upload

---
+++ PizzaProject/Pizza.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Child "Pizza" class that inherits from Main "Food" class 
+/// </summary>
+
+namespace PizzaProject
+{
+    public class Pizza : Food
+    {
+        public Pizza(
+            string typeOfFood,
+            string name,
+            string description,
+            double price
+        ) : base(typeOfFood, name, price)
+        {
+            PizzaCrust = PizzaCrust;
+            PizzaSize = PizzaSize;
+            TypeOfFood = typeOfFood;
+            Description = description;
+        }
+
+        public PizzaCrust PizzaCrust { get; set; }
+        public PizzaSize PizzaSize { get; set; }
+        public string Description { get; set; }
+    }
+}
